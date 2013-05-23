6251642f4b2c9f267c4b12b455ac634016703c31
Koop Otten <koop_otten@hotmail.com>
2013-05-23 17:20:26 +0200

    bier

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,15 @@
 require 'bundler/capistrano'
 load 'deploy/assets'
  
-set :application, "blog"
+set :application, "questionnaire"
 set :ip_address, "149.210.131.239"
  
 set :deploy_to, "/home/deploy/#{application}"
 set :user, 'deploy'
 set :use_sudo, false
- 
+
 set :scm, :git
-set :repository, "git@github.com:NHLHogeschool/blog.git"
+set :repository, "git@github.com:ottenkoop/questionnaire.git"
 set :branch, "master"
 set :repository_cache, "git_cache"
 set :deploy_via, :remote_cache
